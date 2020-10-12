4079c9c13f70d201cc0a8bbe89289a8b25022ebe
Victor Lazaro <lazarofilm@gmail.com>
2020-10-12 17:18:24 -0400

    Updates

---
+++ precache-manifest.77cdffeb950de244dbc1c5f6bdd6b22b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf40ddf72efb5d92f37b4a7aa32e34a",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "42ebd7b8503a8eccf868",
+    "url": "/pomodoro/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b193ceb3b51a0e67e1ec",
+    "url": "/pomodoro/static/js/2.10bac1c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.10bac1c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ebd7b8503a8eccf868",
+    "url": "/pomodoro/static/js/main.76d70e7b.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro/static/media/logo.5d5d9eef.svg"
+  }
+]);
