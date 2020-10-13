8e738af2c6d1739956b4f27e23eded98bab321d6
Victor Lazaro <lazarofilm@gmail.com>
2020-10-13 19:20:21 -0400

    Updates

--- precache-manifest.1beaf37fb7d57c4e444531b975422b1b.js
+++ precache-manifest.1beaf37fb7d57c4e444531b975422b1b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3d44755eb3ed4a4d4ca5f21c87243558",
+    "revision": "5ba3c7bd92557944dcb621f4238c4ccb",
     "url": "/pomodoro/index.html"
   },
   {
-    "revision": "b6e2e75299811affeff8",
+    "revision": "35ff51f308275ae71245",
     "url": "/pomodoro/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "5ab90913871971f3ed78",
+    "revision": "0f58792aa006d916fc43",
     "url": "/pomodoro/static/css/main.830d6b88.chunk.css"
   },
   {
-    "revision": "b6e2e75299811affeff8",
-    "url": "/pomodoro/static/js/2.5c570d1e.chunk.js"
+    "revision": "35ff51f308275ae71245",
+    "url": "/pomodoro/static/js/2.2c94a5e0.chunk.js"
   },
   {
     "revision": "451ffe720aa8add9f554b8ed54913322",
-    "url": "/pomodoro/static/js/2.5c570d1e.chunk.js.LICENSE.txt"
+    "url": "/pomodoro/static/js/2.2c94a5e0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5ab90913871971f3ed78",
-    "url": "/pomodoro/static/js/main.f81d931e.chunk.js"
+    "revision": "0f58792aa006d916fc43",
+    "url": "/pomodoro/static/js/main.5c371412.chunk.js"
   },
   {
     "revision": "ee3590aa5d45d2dacf12",
