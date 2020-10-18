56e0d60d05f7c94612d53ebb5288d05fc622786c
Victor Lazaro <lazarofilm@gmail.com>
2020-10-18 18:04:10 -0400

    Updates

---
+++ precache-manifest.f08fbc509936c55e541d9b6d146df117.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258341bd9ee5e5989bb928cf51bc3f62",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "35a9ffbf09d1c0b03abc",
+    "url": "/pomodoro/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8596545bf4f182430d39",
+    "url": "/pomodoro/static/css/main.85a96d3f.chunk.css"
+  },
+  {
+    "revision": "35a9ffbf09d1c0b03abc",
+    "url": "/pomodoro/static/js/2.cb81c002.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/pomodoro/static/js/2.cb81c002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e384d1baf39ba7fd1a",
+    "url": "/pomodoro/static/js/3.98488f45.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/pomodoro/static/js/3.98488f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8596545bf4f182430d39",
+    "url": "/pomodoro/static/js/main.63c539d7.chunk.js"
+  },
+  {
+    "revision": "b8df715d770364401128",
+    "url": "/pomodoro/static/js/runtime-main.7de2a5b3.js"
+  },
+  {
+    "revision": "a5369aef7df028949bf955c2c93fc3bd",
+    "url": "/pomodoro/static/media/PomodoroTicking.a5369aef.m4a"
+  },
+  {
+    "revision": "7ffd2e73a9fda2a959f7f95cd478d9a4",
+    "url": "/pomodoro/static/media/pomodoro.7ffd2e73.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomodoro/precache-manifest.f5378e4de06c1b4b3b6a68137001b9f7.js"
+  "/pomodoro/precache-manifest.f08fbc509936c55e541d9b6d146df117.js"
 );
 
 self.addEventListener('message', (event) => {
